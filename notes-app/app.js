238d478ce0d672d172e98840552a8cb723b51594
Karan Manglani <bkn45for@gmail.com>
2020-12-06 10:19:44 +0530

    Added search functionality in the notes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,15 @@
 const notes = [
     {
-        title: 'Title 1',
-        body: 'Body 1'
+        title: 'Learn JS',
+        body: 'Learn Javascript for making dynamic websites'
     },
     {
-        title: 'Title 2',
-        body: 'Body 2'
+        title: 'Learn Cryptography',
+        body: 'For personal interest'
     },
     {
-        title: 'Title 3',
-        body: 'Body 3'
+        title: 'Create a cryptography project',
+        body: 'Using python'
     }
 ]
 // // DOM - Object in js that models out html document
@@ -27,6 +27,28 @@ const notes = [
 // document.querySelector('body').appendChild(newParagraph)
 
 // Handeling User Interaction
+
+const filters = {
+    searchText : ''
+}
+
+const renderNotes = function(notes,filters){
+    const filteredNotes = notes.filter(function(note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase()) || note.body.toLowerCase().includes(filters.searchText.toLowerCase())
+
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach(function(note){
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes,filters)
+
 document.querySelector('button').addEventListener('click',function(e){
     console.log('Did this work')
     e.target.textContent = 'This button was clicked'
@@ -39,5 +61,6 @@ document.querySelector('#removeAll').addEventListener('click',function(e){
 })
 
 document.querySelector('#searchText').addEventListener('input',function(e){
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes,filters)
 })
